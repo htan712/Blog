487f264631ca599924950e51b46787ab68a72f08
htan712 <hock.tan712@gmail.com>
2016-09-14 10:46:03 -0700

    removed devise register

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	devise_for :users
     root 'welcome#index'
     get 'welcome/index'
     resources :posts
     resources :projects
     resources :contacts, only: [:new, :create]
+    get '*path' => redirect('/')
 end
